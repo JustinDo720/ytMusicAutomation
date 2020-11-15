906c7ac13d193860df7e82f90b0b918718930f6d
Justin <justindo720@gmail.com>
2020-11-15 16:49:50 -0500

    Removed the download pop up and set the firefox profile to save mp3 files straight to the downloads folder

--- musicDownload.py
+++ musicDownload.py
@@ -1,5 +1,6 @@
 # This is the where we download the music request by the user
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,10 +17,19 @@ yt_converter_url = 'https://ytmp3.cc/en13/'
 yt_converter_download_button = '/html/body/div[2]/div[1]/div[1]/div[3]/a[1]'
 testing_youtube_url = 'https://www.youtube.com/watch?v=gD7lUu-SRwY&list=RDgD7lUu-SRwY&index=1&ab_channel=MixHound'
 
+# FireFox Preferences for Selenium
+fp = webdriver.FirefoxProfile()
+fp.set_preference('browser.download.dir', f'{os.getcwd()}/music_downloads/')
+fp.set_preference('browser.download.manager.showWhenStarting', False)
+fp.set_preference('browser.download.folderList', 2)
+# This preference ignores the download tab that asks what to do with mp3 files
+fp.set_preference('browser.helperApps.neverAsk.saveToDisk', '.mp3 audio/mpeg')
+
+
 # We are going to request a url from users and use that url to download using a youtube to mp3 converter
-# yt_url = input("Please enter a youtube url of your music: ")
+#yt_url = input("Please enter a youtube url of your music: ")
 yt_url = testing_youtube_url
-web = webdriver.Firefox(executable_path=geckodriver_path)
+web = webdriver.Firefox(executable_path=geckodriver_path, firefox_profile=fp)
 web.get(yt_converter_url)
 
 # Start putting the url and downloading the mp3 version of the yt link
@@ -31,9 +41,11 @@ web.find_element_by_id('submit').click()
 try:
     WebDriverWait(web, 40).until(EC.visibility_of_all_elements_located(('tag name','a')))
     web.find_element_by_xpath(yt_converter_download_button).click()
+    time.sleep(10)
     web.quit()
 except Exception:
     print('We apologize but your download exceeded 40 seconds... We will wait for another 40 seconds')
     WebDriverWait(web, 40).until(EC.visibility_of_all_elements_located(('tag name', 'a')))
     web.find_element_by_xpath(yt_converter_download_button).click()
     web.quit()
+
