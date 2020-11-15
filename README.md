fdf7fc4662f97a43441448a79cdeed8e2b38a0e9
Justin <justindo720@gmail.com>
2020-11-15 15:59:10 -0500

    Updated the readme syntax by removing the ling on description. We waited for the download using WebDriverWait.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Youtube Music Download Automation
 
-## Description:
-  We want to allow users to get mp3 versions of their youtube video. The user just has to put
-  in their youtube link and Selenium will do the work.
+### Description:
+We want to allow users to get mp3 versions of their youtube video. The user just has to put
+in their youtube link and Selenium will do the work.
 
 ### Goals:
 1. To clutter or false downloads
@@ -10,6 +10,7 @@
 1. No pop ups. We want a robust program that waits for the download 
 
 ### Ideas:
+<!-- This has to be spaced out -->
 * [ ] Instead of a link can we go for maybe a title and the user gets to choose?
 * [ ] Hook the selenium program to a gui like Tkinter
 * [ ] Chrome or Firefox? Basically make the program more robust 
--- musicDownload.py
+++ musicDownload.py
@@ -1,6 +1,8 @@
 # This is the where we download the music request by the user
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 import time
 '''
@@ -11,6 +13,7 @@ Path Section:
 '''
 geckodriver_path = os.environ.get('GECKODRIVER')
 yt_converter_url = 'https://ytmp3.cc/en13/'
+yt_converter_download_button = '/html/body/div[2]/div[1]/div[1]/div[3]/a[1]'
 testing_youtube_url = 'https://www.youtube.com/watch?v=gD7lUu-SRwY&list=RDgD7lUu-SRwY&index=1&ab_channel=MixHound'
 
 # We are going to request a url from users and use that url to download using a youtube to mp3 converter
@@ -23,11 +26,14 @@ web.get(yt_converter_url)
 web.find_element_by_id('input').send_keys(yt_url)
 web.find_element_by_id('submit').click()
 
-# Gives 10 seconds to convert and download
-try:
-    web.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div[3]/a[1]').click()
-except Exception:
-    time.sleep(10)
-    web.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div[3]/a[1]').click()
+# This is where we want to wait for the download button
 
-web.quit()
+try:
+    WebDriverWait(web, 40).until(EC.visibility_of_all_elements_located(('tag name','a')))
+    web.find_element_by_xpath(yt_converter_download_button).click()
+    web.quit()
+except Exception:
+    print('We apologize but your download exceeded 40 seconds... We will wait for another 40 seconds')
+    WebDriverWait(web, 40).until(EC.visibility_of_all_elements_located(('tag name', 'a')))
+    web.find_element_by_xpath(yt_converter_download_button).click()
+    web.quit()
